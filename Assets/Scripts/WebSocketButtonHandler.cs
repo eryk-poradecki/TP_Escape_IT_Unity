9ea55cfee67f8963b259a6f32a9b9aa3d8c7bcbe
Dawid Borkowski 242210 <242210@edu.p.lodz.pl>
2023-06-18 11:06:41 +0200

    Unity now send help request on button click and downloads and plays audio file after receiving audio_ready message on WebSocket

--- Assets/Scripts/WebSocketButtonHandler.cs
+++ Assets/Scripts/WebSocketButtonHandler.cs
@@ -4,50 +4,101 @@ using UnityEngine;
 using UnityEngine.UI;
 using WebSocketSharp;
 using Newtonsoft.Json;
-
+using System.IO;
+using UnityEngine.Networking;
 
 public class WebSocketButtonHandler : MonoBehaviour
 {
-    WebSocket ws;
-    Button webSocketButton;
+    private string characterModelToAssign = "Winston";
+    private GameObject characterModel;
+    private AudioSource audioSource;
+    public WebSocket webSocket;
+    private Button webSocketButton;
+ 
+	private class WebSocketMessage
+	{
+		public string type;
+		public string message;	
+	}
 
     void Start()
     {
-        Debug.Log("Trying to connect! --WebSocketSharp");
+        characterModel = GameObject.Find(characterModelToAssign);
+        audioSource = characterModel.GetComponent<AudioSource>();
 
-        ws = new WebSocket("ws://localhost:8000/ws/socket-server/");
+        Debug.Log("Initialization started!");
 
-        ws.OnOpen += (sender, e) =>
+        webSocket = new WebSocket("ws://localhost:8000/ws/socket-server/");
+        
+        webSocket.OnOpen += (sender, e) =>
         {
-            Debug.Log("WebSocket connection open --WebSocketSharp");
+            Debug.Log("WebSocket connection opened!");
         };
-
-        ws.OnMessage += (sender, e) =>
+        
+        webSocket.OnMessage += (sender, e) =>
         {
-            Debug.Log("Message received! --WebSocketSharp from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            Debug.Log("Message received! from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            
+			WebSocketMessage message = JsonConvert.DeserializeObject<WebSocketMessage>(e.Data);
 
+			Debug.Log(message.type);
+			Debug.Log(message.message);
+
+			if(message.type == "audio_ready")
+			{
+            	UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            	{
+                	StartCoroutine(GetAudioClip());
+           		});
+			}
+            
+            Debug.Log("Receiving Ended!");            
         };
-
-        ws.Connect();
-
-        webSocketButton = GetComponent<Button>();
+        
+		webSocketButton = GetComponent<Button>();
         webSocketButton.onClick.AddListener(WebSocketButtonClickHandler);
+
+        webSocket.Connect();
     }
 
+    private IEnumerator GetAudioClip()
+    {
+        Debug.Log("Inside GetAudioClip");
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("http://localhost:8000/audio/", AudioType.MPEG))
+        {
+            Debug.Log("BeforeRequest");
+            yield return www.SendWebRequest();
+            Debug.Log("AfterRequest");
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                AudioClip myClip = DownloadHandlerAudioClip.GetContent(www);
+                audioSource.clip = myClip;
+                audioSource.Play();
+            }
+        }
+    }
+    
     private void WebSocketButtonClickHandler()
     {
         var message = new Dictionary<string, string>
         {
-            { "hint", "I need help!" }
+            { "type", "help_request" }
         };
 
         string messageJSON = JsonConvert.SerializeObject(message);
-        ws.Send(messageJSON);
+
+        webSocket.Send(messageJSON);
     }
 
-    private void OnDestroy()
+    void OnDestroy()
     {
-        ws.Close();
-        Debug.Log("Connection closed! --WebSocketSharp");
+        webSocket.Close();
+        Debug.Log("Connection closed!");
     }
+    
 }
